1feec20f40ed98fd876a8a5d18885a304d4a4d12
Jasmine Yuen <jyuen30@stuy.edu>
2022-09-22 09:17:44 -0400

    adding krewes.py

---
+++ 04_choose/krewes.py
@@ -0,0 +1,21 @@
+"""
+Jasmine Yuen
+SoftDev
+K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+2022-09-22
+time spent: <elapsed time in hours, rounded to nearest tenth>
+"""
+
+import random
+
+krewes = {
+    2 : ["devo1", "devo2", "devo3"],
+    7 : ["devo1", "devo2", "devo3"],
+    8 : ["devo1", "devo2", "devo3"]
+}
+
+rrow = random.choice(krewes.keys())
+#rcol = random.randrange(len(krewes[rrow]))
+
+print(rrow)
+#print(rcol)
