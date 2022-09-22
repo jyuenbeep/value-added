871c15250bbb0b1f9b3ae987a0dba5919f05465c
Jasmine Yuen <jyuen3@stuy.edu>
2022-09-21 23:53:45 -0400

    added for list1

--- 03_py/list1/make_pi.py
+++ 03_py/list1/make_pi.py
@@ -1,3 +1,12 @@
+# https://codingbat.com/prob/p113659
+
+"""
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+"""
+
 def make_pi():
   pi = [3, 1, 4]
   return pi
+
+  # test cases: do not edit
+print(make_pi()) # [3, 1, 4]
