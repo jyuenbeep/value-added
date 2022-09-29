c0de2306b9d641c73894ef115e226ba89237050f
Jasmine Yuen <jyuen30@stuy.edu>
2022-09-29 09:07:44 -0400

    almost done

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -9,8 +9,9 @@ ary = s.split("@@@")
 
 for i in len(ary):
     ary[i] = ary[i].split("$$$")
-    for index in len(ary[i]):
-        ary[i][index]
+    period = ary[i][0]
+    #retreiving the period from the subarray
+    krewes[period] = ary[i][1]:ary[i][2]
 
 rrow = random.choice(list(krewes.keys()))
 rcol = random.choice(krewes[rrow])
