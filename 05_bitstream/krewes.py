275def45615f757b55d1df1b8fc217bee4420db1
Jasmine Yuen <jyuen30@stuy.edu>
2022-09-30 09:06:33 -0400

    trying something new

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -8,11 +8,18 @@ krewes = {}
 s = "2$$$jasmine$$$duck@@@2$$$jeffrey$$$ducky@@@8$$$daniel$$$bob"
 
 ary = s.split("@@@")
+plist = []
 
 for i in range(len(ary)):
     ary[i] = ary[i].split("$$$")
-    period = ary[i][0]
-    krewes[period].update({ary[i][1]:ary[i][2]})
+    if plist.index(ary[i][0])!=-1:
+        plist+=ary[i][0]
+
+for num in plist:
+    krewes[num] = []
+    
+for i in ary:
+    krewes[i[0]] += [i[1], i[2]]
 
 # rrow = (list(krewes.keys()))
 # print(f"{rrow} : {rcol}")
