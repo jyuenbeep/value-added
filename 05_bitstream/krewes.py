c9d34df311109b70d3a9ea5c9079102b25441005
Jasmine Yuen <jyuen30@stuy.edu>
2022-09-30 09:20:44 -0400

    heading done

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,3 +1,15 @@
+"""
+Jasmine Yuen & Jeffrey Zou (Temporary Ducks)
+SoftDev
+K05 -- Writing a Python program to read a .txt file and populate
+a dictionary. Then randomly select a Devo and print the information.
+2022-09-29
+time spent: 1
+
+DISCO: We learned how to append to a dictionary and read from a .txt file.
+QCC: NONE
+"""
+
 import random
 
 krewes = {}
@@ -8,7 +20,7 @@ f.close()
 
 ary = s.split("@@@")
 
-# grabbing all the different periods from the array
+# grabbing all the different periods from the array and putting the key into krewes
 for i in range(len(ary)):
     ary[i] = ary[i].split("$$$")
     if ary[i][0] not in krewes:
