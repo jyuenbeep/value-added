6b289492a1f92ea2c23ff8b5b9143d5f1476170d
Jasmine Yuen <jyuen30@stuy.edu>
2022-09-30 09:11:46 -0400

    done

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -8,18 +8,16 @@ krewes = {}
 s = "2$$$jasmine$$$duck@@@2$$$jeffrey$$$ducky@@@8$$$daniel$$$bob"
 
 ary = s.split("@@@")
-plist = []
 
+# grabbing all the different periods from the array
 for i in range(len(ary)):
     ary[i] = ary[i].split("$$$")
-    if plist.index(ary[i][0])!=-1:
-        plist+=ary[i][0]
-
-for num in plist:
-    krewes[num] = []
+    if ary[i][0] not in krewes:
+        krewes[ary[i][0]] = []
     
+# appending each person to their specified key
 for i in ary:
-    krewes[i[0]] += [i[1], i[2]]
+    krewes[i[0]].append([i[1], i[2]])
 
 # rrow = (list(krewes.keys()))
 # print(f"{rrow} : {rcol}")
