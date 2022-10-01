678db782cf97f41de8b45c5d6c6213da1f62ee4c
Jasmine Yuen <jyuen3@stuy.edu>
2022-10-01 18:42:07 -0400

    added process of krewes.py

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -8,6 +8,17 @@ time spent: 1
 
 DISCO: We learned how to append to a dictionary and read from a .txt file.
 QCC: NONE
+
+PROCESS:
+1. import random library
+2. initialize a krewes dictionary
+3. read the .txt file into a string format
+4. split the string into each person by .split("@@@")
+5. split the string into each person's attributes by .split("$$$")
+6. initialize each distinct period as a dictionary key
+7. append the elements of each person into their respective dictionary lists
+8. choose random person
+9. print 
 """
 
 import random
@@ -25,7 +36,7 @@ for i in range(len(ary)):
     ary[i] = ary[i].split("$$$")
     if ary[i][0] not in krewes:
         krewes[ary[i][0]] = []
-    
+
 # appending each person to their specified key
 for i in ary:
     krewes[i[0]].append([i[1], i[2]])
