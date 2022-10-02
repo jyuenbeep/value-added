376e90d5f22cf45913378e2b5ed77183bc20e882
Jasmine Yuen <jyuen3@stuy.edu>
2022-10-02 01:47:57 -0400

    input it all into a dictionary and am trying to get rid of the total row

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,7 +1,17 @@
 import csv
+
+# initializing dictionary + keys
+jobDict = {}
+jobDict["Job Class"] = []
+jobDict["Percentage"] = []
+
+# opening .csv file and reading it as a .DictReader object
+# adding the job class and percentage to their appropriate slots in the dictionary
 with open('occupations.csv') as f:
     r =  csv.DictReader(f)
-    # for row in r:
-    #     print(row)
+    for row in r:
+        jobDict["Job Class"].append(row['Job Class'])
+        jobDict["Percentage"].append(row['Percentage'])
 
-print(r)
+# testing
+print(jobDict)
