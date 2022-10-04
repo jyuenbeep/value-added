0e9af3c89136823d4675404a58c0f5550fc15ff2
Jasmine Yuen <jyuen30@stuy.edu>
2022-10-04 09:08:19 -0400

    done

--- 07_app/app.py
+++ 07_app/app.py
@@ -24,13 +24,17 @@ DISCO:
 QCC:
 0. Python.
 1. It's in the link. It shows the number of the local host that serves your webpage.
-2. This prints to the terminal --- " Serving Flask app 'app' ".
+It is in the path of the webpage.
+2. This prints to the terminal --- " Serving Flask app 'app' "s.
 3. This prints the name of the Flask app, which is by default the name of the file.
-4.  
-5.
+4. The return prints to the website because it is returning a value to the Flask function.
+5. app.run() just runs the function. It is very similar to other languages, such as Python,
+except Python doesn't use the word run.
 ...
 INVESTIGATIVE APPROACH:
-<Your concise summary of how
- you and your team set about
- "illuminating the cave of ignorance" here...>
+1. I downloaded Flask.
+2. I ran the file.
+3. I looked at the output and answered the questions.
+4. I compared with my teammates to solidy by answers.
+5. I gained a better understanding of Flask and revised by answers.
 '''
