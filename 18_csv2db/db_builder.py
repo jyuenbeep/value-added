731c503c61e3f3ba9216eacebb7b48088b51f3bb
Jasmine Yuen <jyuen30@stuy.edu>
2022-10-26 09:45:12 -0400

    f string is broken :(

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -3,6 +3,16 @@
 #skeleton/stub :: SQLITE3 BASICS
 #Oct 2022
 
+"""
+INSTRUCTIONS:
+
+Your trio MISSION: Write a simple Python script to import CSV data into a relational database.
+
+Read data from CSV files, and create a database whose table structure mimics that of the CSV files.
+(In the care package you will find a CSV file of students and their IDs, and another linking said IDs to the students’ 
+current grades in some courses. Desire more lulz? Add extra, more expansive CSVs.)
+"""
+
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
@@ -24,9 +34,44 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 - 
 """
 
+db.execute("DROP TABLE if exists students")
+db.execute("DROP TABLE if exists courses")
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+students = {}
+students['name'] = []
+students['age'] = []
+students['id'] = []
+
+courses = {}
+courses['code'] = []
+courses['mark'] = []
+courses['id'] = []
+
+with open('courses.csv') as f:
+    r = csv.DictReader(f)
+    for row in r:
+        courses['code'].append(row['code'])
+        courses['mark'].append(row['mark'])
+        courses['id'].append(row['id'])
+
+with open('students.csv') as f:
+    r = csv.DictReader(f)
+    for row in r:
+        students['name'].append(row['name'])
+        students['age'].append(row['age'])
+        students['id'].append(row['id'])
+
+createCourses = "CREATE TABLE courses (code STRING, mark INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
+createStudents = "CREATE TABLE students (name STRING, age INTEGER, id INTEGER);"
+
+c.execute(createCourses)    # run SQL statement
+c.execute(createStudents)
+
+for i in range(len(students)):
+    c.execute("INSERT INTO students VALUES (students['name'][{i}], students['age'][{i}], students['id'][{i}]);")
+
+for i in range(len(courses)):
+    c.execute(f"INSERT INTO students VALUES (courses['code'][{i}], courses['mark'][{i}], courses['id'][{i}]);")
 
 #==========================================================
 
