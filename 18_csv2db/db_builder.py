28501625f564b7c4acc57dd27aeed7fbdb7454fa
Jasmine Yuen <jyuen3@stuy.edu>
2022-10-27 01:55:30 -0400

    dbbuilder done

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,5 +1,5 @@
 #Clyde "Thluffy" Sinclair
-#SoftDev  
+#SoftDev
 #skeleton/stub :: SQLITE3 BASICS
 #Oct 2022
 
@@ -9,7 +9,7 @@ INSTRUCTIONS:
 Your trio MISSION: Write a simple Python script to import CSV data into a relational database.
 
 Read data from CSV files, and create a database whose table structure mimics that of the CSV files.
-(In the care package you will find a CSV file of students and their IDs, and another linking said IDs to the students’ 
+(In the care package you will find a CSV file of students and their IDs, and another linking said IDs to the students’
 current grades in some courses. Desire more lulz? Add extra, more expansive CSVs.)
 """
 
@@ -31,18 +31,20 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 """
 - command --> CREATE TABLE
 - open csv file w dictreader
-- 
+-
 """
 
 db.execute("DROP TABLE if exists students")
 db.execute("DROP TABLE if exists courses")
 
 students = {}
+studentCount = 0
 students['name'] = []
 students['age'] = []
 students['id'] = []
 
 courses = {}
+courseCount = 0
 courses['code'] = []
 courses['mark'] = []
 courses['id'] = []
@@ -53,6 +55,7 @@ with open('courses.csv') as f:
         courses['code'].append(row['code'])
         courses['mark'].append(row['mark'])
         courses['id'].append(row['id'])
+        courseCount+=1
 
 with open('students.csv') as f:
     r = csv.DictReader(f)
@@ -60,22 +63,37 @@ with open('students.csv') as f:
         students['name'].append(row['name'])
         students['age'].append(row['age'])
         students['id'].append(row['id'])
+        studentCount+=1
 
-createCourses = "CREATE TABLE courses (code STRING, mark INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
-createStudents = "CREATE TABLE students (name STRING, age INTEGER, id INTEGER);"
+createCourses = "CREATE TABLE courses (code STRING, mark INTEGER, id INTEGER)"          # test SQL stmt in sqlite3 shell, save as string
+createStudents = "CREATE TABLE students (name STRING, age INTEGER, id INTEGER)"
 
 c.execute(createCourses)    # run SQL statement
 c.execute(createStudents)
 
-for i in range(len(students)):
-    c.execute(f"INSERT INTO students VALUES (students['name'][{i}], students['age'][{i}], students['id'][{i}]);")
+for i in range(studentCount):
+    name = students['name'][i]
+    age = students['age'][i]
+    id = students['id'][i]
+    c.execute(f"INSERT INTO students VALUES('{name}', {age}, {id})")
 
-for i in range(len(courses)):
-    c.execute(f"INSERT INTO students VALUES (courses['code'][{i}], courses['mark'][{i}], courses['id'][{i}]);")
+# # testing
+# c.execute("SELECT * FROM students")
+# var = c.fetchall()
+# print (var)
+
+for i in range(courseCount):
+    code = courses['code'][i]
+    mark = courses['mark'][i]
+    id = courses['id'][i]
+    c.execute(f"INSERT INTO courses VALUES('{code}', {mark}, {id})")
+
+# # testing
+# c.execute("SELECT * FROM courses")
+# var = c.fetchall()
+# print (var)
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
