067289815a0ada01e4633aa81e936347bf3ed5be
Jasmine Yuen <jyuen3@stuy.edu>
2022-11-05 18:05:29 -0400

    i hate cs

--- 19_session/app.py
+++ 19_session/app.py
@@ -3,38 +3,33 @@ Blue Haired Gals With Pronouns: Jasmine Yuen, Talia Hsia, Ziying Jian
 SoftDev
 K19 - Flask app that uses session capabilites to allow user to login and logout
 2022-11-03
-time spent: 
+time spent:
 """
 
-from flask import Flask            
-from flask import render_template   
-from flask import request 
+from flask import request, Flask, render_template, redirect
 from flask import session
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 username = "ziying"
 password = "john"
+
+#app.secret_key = '74f9cf8fcf367b3c88469c1e720934b2298c4108928e85d4f79703f763942bf3'
 
 def authenticate(user, passw):
     return (user == username and passw == password)
 
 @app.route("/", methods=['GET', 'POST'])
-def disp_loginpage():
-    return render_template( 'login.html' )
+def index():
+    return render_template("login.html", errorLogin = "")
 
 @app.route("/login", methods=['GET', 'POST'])
-def response_template(): 
+def login():
     if authenticate(request.args['username'], request.args['password']):
-        return render_template('response.html', um="this works")  
-    return render_template('response.html', um="this doesn't work")  
+        return render_template('response.html', functional="WORKS!")
+    else:
+        return render_template('login.html', errorLogin = "Bad password or bad username.")
 
-# @app.route('/logout')
-# def logout():
-#     # remove the username from the session if it's there
-#     session.pop('username', None)
-#     return redirect(url_for('index'))
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.debug = True
     app.run()
