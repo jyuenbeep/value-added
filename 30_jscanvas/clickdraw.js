645592fa7ff628a48b7b5e3f28a867956cc47fd5
Jasmine Yuen <jyuen30@stuy.edu>
2023-04-24 12:21:56 -0400

    adding and started 30

---
+++ 30_jscanvas/clickdraw.js
@@ -0,0 +1,65 @@
+// TOOLBOX:
+// clearRect()
+// getElementById()
+// addEventListener()
+// fillStyle()
+// strokeStyle()
+// clearRect()
+// fillText()
+// beginPath()
+// fillStype
+// arc()
+// fill()
+// stroke()
+// Math.PI
+// offsetX
+// offsetY
+
+//retrive node in DOM via ID
+var c = document.getElementById("slate");
+
+//instantate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+//init global state var 
+var mode = "rect";
+
+//var toggleMode = function(e) {
+var toggleMode = (e) => {
+    console.log("toggling...");
+    if () {
+
+    }
+    else {
+
+    }
+}
+
+// var drawRect = function(e) {
+//     var mouseX = ;
+//     var mouseY = ;
+//     console.log("mouseclick registared at ", mouseX, mouseY);
+// }
+
+// //var drawCircle = function(e) {
+// var drawCircle = (e) => {
+    
+//     console.log("mouseclick registered at ", mouseX, mouseY);
+
+// }
+
+// //var draw = function(e) {
+// var draw = (e) => {
+
+// }
+
+// //var wipeCanvas = function() {
+// var wipeCanvas = () => {
+
+// }
+
+// c.addEventListener("click", draw);
+// var bToggler = document. ;
+// bToggler. ;
+// var clearB = ;
+// clearB.;
