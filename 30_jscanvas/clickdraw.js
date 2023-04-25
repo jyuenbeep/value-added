419053c077a10c6994a544d0b29e8c11c82ccd8a
Jasmine Yuen <jyuen30@stuy.edu>
2023-04-25 09:02:45 -0400

    finished drawing

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -27,39 +27,46 @@ var mode = "rect";
 //var toggleMode = function(e) {
 var toggleMode = (e) => {
     console.log("toggling...");
-    if () {
-
+    if (mode === "rect") {
+        mode = "circ";
     }
     else {
-
+        mode = "rect";
     }
 }
 
-// var drawRect = function(e) {
-//     var mouseX = ;
-//     var mouseY = ;
-//     console.log("mouseclick registared at ", mouseX, mouseY);
-// }
+var drawRect = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;    
+    ctx.fillRect(mouseX, mouseY, 10, 10);
+    console.log("mouseclick registered at ", mouseX, mouseY);
+}
 
-// //var drawCircle = function(e) {
-// var drawCircle = (e) => {
-    
-//     console.log("mouseclick registered at ", mouseX, mouseY);
+var drawCircle = function(e){
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 10, 0, 2 * Math.PI);
+    ctx.fill();
+}
 
-// }
+//var draw = function(e) {
+var draw = (e) => {
+    if (mode == "rect") {
+        drawRect(e);
+    } 
+    else {
+        drawCircle(e);
+    }
+}
 
-// //var draw = function(e) {
-// var draw = (e) => {
+//var wipeCanvas = function() {
+var wipeCanvas = () => {
+    ctx.clearRect(0, 0, 600, 600);
+}
 
-// }
-
-// //var wipeCanvas = function() {
-// var wipeCanvas = () => {
-
-// }
-
-// c.addEventListener("click", draw);
-// var bToggler = document. ;
-// bToggler. ;
-// var clearB = ;
-// clearB.;
+c.addEventListener("click", draw);
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+var bClear = document.getElementById("buttonClear");
+bClear.addEventListener("click", wipeCanvas);
